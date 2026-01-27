90f6fddc1fb521b3ab6dc8c1032dc405d0ac5997
Helsingbuss <info@helsingbuss.se>
2026-01-27 18:02:11 +0100

    Fix import path for UpcomingTripsSection

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,7 +1,7 @@
-import HeroHeader from "../components/home/HeroHeader";
+﻿import HeroHeader from "../components/home/HeroHeader";
 import SectionTitle from "../components/sections/SectionTitle";
 import ServiceCards from "../components/sections/ServiceCards";
-import UpcomingTripsSection from "@/components/sections/UpcomingTripsSection";
+import UpcomingTripsSection from "../components/sections/UpcomingTripsSection";
 
 export default function Page() {
   return (
